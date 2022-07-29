d0599779a0501b6fc2b7c217311863393ab155f9
Migheli <98918370+Migheli@users.noreply.github.com>
2022-07-29 10:50:32 +0300

    Replace fb handlers from fb_bot

--- fb-bot.py
+++ fb-bot.py
@@ -1,18 +1,21 @@
 import os
-from functools import partial
-import redis
-import requests
-from flask import Flask, request
-from moltin_api_handlers import get_token_dataset, get_file_url, add_product_to_cart, get_cart_items, delete_item_from_cart, get_cart_by_reference, get_product_catalogue, check_token_status
 import json
-import time
 import logging
+from functools import partial, update_wrapper
+import redis
+from flask import Flask, request
+
+from moltin_api_handlers import get_token_dataset, get_file_url, add_product_to_cart, get_cart_items,\
+    delete_item_from_cart, get_cart_by_reference, get_product_catalogue, check_token_status
 from cached_menu_handlers import get_categorised_products_set, get_cached_products_by_category_id, categories_id
-import functools
+
+from fb_requests_handlers import send_message, send_gallery, get_menu_elements
+
 
 logger = logging.getLogger(__name__)
 
 _database = None
+
 
 def get_database_connection():
     """
@@ -29,23 +32,20 @@ def get_database_connection():
 
 def handle_start(sender_id, moltin_token_dataset, message_content, menu):
     target_category_id = os.environ['BASIC_CATEGORY_ID']
-    send_menu(sender_id, moltin_token_dataset, target_category_id, menu)
+    send_menu_by_category(sender_id, target_category_id, menu)
     return 'MENU'
 
 
-def send_menu(sender_id, moltin_token_dataset, target_category_id, menu):
-    # send menu here
+def send_menu_by_category(sender_id, target_category_id, menu):
     recipient_id = sender_id
     categorised_products = get_categorised_products_set(menu['data'])
     cached_menu = get_cached_products_by_category_id(categorised_products, target_category_id)
-    print(f'Кэшированное меню внутри send_menu{cached_menu}')
     elements = get_menu_elements(cached_menu)
     send_gallery(recipient_id, elements)
 
 
 def handle_menu(sender_id, moltin_token_dataset, message_content, menu):
 
-    print(f'СОДЕРЖАНИЕ СООБЩЕНИЯ {message_content}')
     status, action, payload = message_content.split('::')
     cart_id = f'cart_{sender_id}'
     recipient_id = sender_id
@@ -59,12 +59,11 @@ def handle_menu(sender_id, moltin_token_dataset, message_content, menu):
             send_gallery(recipient_id, elements)
         if action == 'send_category_menu':
             target_category_id = categories_id[payload]
-            send_menu(recipient_id, moltin_token_dataset, target_category_id, menu)
+            send_menu_by_category(recipient_id, target_category_id, menu)
         if action == 'add':
             add_product_to_cart(moltin_token_dataset, payload, cart_id)
             send_message(sender_id, 'Товар добавлен в корзину')
         return 'MENU'
-
 
     if status == 'in_cart_menu':
         if action == 'add':
@@ -73,7 +72,7 @@ def handle_menu(sender_id, moltin_token_dataset, message_content, menu):
             delete_item_from_cart(moltin_token_dataset, cart_id, payload)
         if action == 'to_menu':
             target_category_id = categories_id['basic']
-            send_menu(recipient_id, moltin_token_dataset, target_category_id, menu)
+            send_menu_by_category(recipient_id, target_category_id, menu)
 
         return 'MENU'
 
@@ -89,7 +88,6 @@ def handle_users_reply(sender_id, moltin_token_dataset, message_text, menu):
 
     type, action, payload = message_text.split('::')
 
-
     states_functions = {
         'START': handle_start,
         'MENU': handle_menu,
@@ -98,10 +96,9 @@ def handle_users_reply(sender_id, moltin_token_dataset, message_text, menu):
     if not recorded_state or recorded_state.decode("utf-8") not in states_functions.keys():
         user_state = "START"
     else:
-       user_state = recorded_state.decode("utf-8")
+        user_state = recorded_state.decode("utf-8")
     if payload == "/start":
         user_state = "START"
-    print(user_state)
     state_handler = states_functions[user_state]
     next_state = state_handler(sender_id, moltin_token_dataset, message_text, menu)
     db.set(sender_id, next_state)
@@ -119,19 +116,6 @@ def verify():
     return "Hello world", 200
 
 
-def set_main_img_href(product_dataset, moltin_token_dataset):
-    product_img_id = product_dataset['relationships']['main_image']['data']['id']
-    product_dataset['relationships']['main_image']['data']['href'] = get_file_url(moltin_token_dataset, product_img_id)
-
-
-def update_database(db, moltin_token_dataset):
-    menu = get_product_catalogue(moltin_token_dataset)
-    for product_dataset in menu['data']:
-        set_main_img_href(product_dataset, moltin_token_dataset)
-    menu = json.dumps(menu)
-    db.set('menu', menu)
-
-
 def get_moltin_changes(db, moltin_token_dataset):
     """
         Вебхук, который обрабатывает уведомления об обновлениях от интеграции Moltin.
@@ -141,11 +125,8 @@ def get_moltin_changes(db, moltin_token_dataset):
     moltin_token_dataset = check_token_status(moltin_token_dataset)
     data = request.get_json()
     if data.get("integration"):
-        print(f"Сработала интеграция {data['integration']}")
         if data['integration']['id'] == os.environ["MOLTIN_WEBHOOK_INTEGRATION_ID"]:
-            print(f"Сработала проверка интеграции {data['integration']}")
             update_database(db, moltin_token_dataset)
-
 
             return "ok", 200
 
@@ -163,232 +144,37 @@ def facebook_webhook(db, moltin_token_dataset):
     data = request.get_json()
 
     if data["object"] == "page":
-        print(f"Сработала страница")
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
                 print(entry["messaging"])
                 sender_id = messaging_event["sender"]["id"]
-                #db.set(sender_id, "START")
                 if (sender_id == '5304713252982644') and not (messaging_event.get('delivery') or messaging_event.get('read')):
                     print('Сработало сообщение от пользователя')
                     if messaging_event.get("message"):
                         message_content = f'text_message::0::{messaging_event["message"]["text"]}'
                     if messaging_event.get('postback'):
                         message_content = messaging_event['postback']['payload']
-                    print(message_content)
                     handle_users_reply(sender_id, moltin_token_dataset, message_content, menu)
     return "ok", 200
 
 
-def send_message(recipient_id, message_text):
-    params = {"access_token": os.environ["PAGE_ACCESS_TOKEN"]}
-    headers = {"Content-Type": "application/json"}
-    request_content = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "text": message_text
-        }
-    })
-    response = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers,
-                             data=request_content)
-    response.raise_for_status()
 
-
-def get_main_menu():
-    return {
-        "title": "Меню",
-        "image_url": os.environ["PIZZERIA_LOGO_URL"],
-        "subtitle": "Здесь можно выбрать один из вариантов",
-        "default_action": {
-            "type": "web_url",
-            "url": os.environ["PIZZERIA_LOGO_URL"],
-            "webview_height_ratio": "compact",
-        },
-        "buttons": [
-            {
-                "type": "postback",
-                "title": "Сделать заказ",
-                "payload": "in_menu::make_order::0"
-            },
-            {
-                "type": "postback",
-                "title": "Корзина",
-                "payload": "in_menu::at_cart::0"
-            },
-            {
-                "type": "postback",
-                "title": "Акции",
-                "payload": "in_menu::promo::0"
-            }
-        ]
-    }
-
-def get_categories_menu():
-    return {
-        "title": "Не нашли нужную пиццу?",
-        "image_url": os.environ["CATEGORIES_LOGO_URL"],
-        "subtitle": "Здесь можно выбрать один из вариантов",
-        "default_action": {
-            "type": "web_url",
-            "url": os.environ["PIZZERIA_LOGO_URL"],
-            "webview_height_ratio": "compact",
-        },
-        "buttons": [
-            {
-                "type": "postback",
-                "title": "Острые",
-                "payload": f"in_menu::send_category_menu::spicy"
-            },
-            {
-                "type": "postback",
-                "title": "Сытные",
-                "payload": f"in_menu::send_category_menu::nutritious"
-            },
-            {
-                "type": "postback",
-                "title": "Особые",
-                "payload": f"in_menu::send_category_menu::specials"
-            }
-        ]
-    }
-
-
-def get_serialized_product_dataset(product_dataset):
-
+def set_main_img_href(product_dataset, moltin_token_dataset):
     product_img_id = product_dataset['relationships']['main_image']['data']['id']
-    product_img_url = product_dataset['relationships']['main_image']['data']['href']
-    print(product_dataset)
-    serialize_product_dataset = {
-        "title": f"{product_dataset['name']} ({product_dataset['price'][0]['amount']} руб.)",
-        "image_url": product_img_url,
-        "subtitle": product_dataset['description'],
-        "default_action": {
-            "type": "web_url",
-            "url": "https://www.originalcoastclothing.com/",
-            "webview_height_ratio": "compact",
-        },
-        "buttons": [
-                      {
-                        "type":"postback",
-                        "title":"В корзину",
-                        "payload": f"in_menu::add::{product_dataset['id']}"
-                      }
-        ]
-    }
-
-    return serialize_product_dataset
+    product_dataset['relationships']['main_image']['data']['href'] = get_file_url(moltin_token_dataset, product_img_id)
 
 
-def get_serialized_cart_item_dataset(product_dataset):
+def update_database(db, moltin_token_dataset):
+    menu = get_product_catalogue(moltin_token_dataset)
+    for product_dataset in menu['data']:
+        set_main_img_href(product_dataset, moltin_token_dataset)
+    menu = json.dumps(menu)
+    db.set('menu', menu)
 
-    print(product_dataset)
-    serialize_product_dataset = {
-        "title": f"{product_dataset['name']}. В корзине: {product_dataset['quantity']} шт.",
-        "image_url": product_dataset['image']['href'],
-        "subtitle": f"{product_dataset['description']}",
-        "default_action": {
-            "type": "web_url",
-            "url": "https://www.originalcoastclothing.com/",
-            "webview_height_ratio": "compact",
-        },
-        "buttons": [
-                      {
-                        "type":"postback",
-                        "title":"Добавить еще одну",
-                        "payload": f"in_cart_menu::add::{product_dataset['product_id']}"
-                      },
-
-                      {
-                          "type": "postback",
-                          "title": "Удалить из корзины",
-                          "payload": f"in_cart_menu::replace::{product_dataset['id']}"
-                      },
-
-        ]
-    }
-
-    return serialize_product_dataset
-
-def get_cart_page(cart_price):
-    return {
-        "title": "Корзина",
-        "image_url": os.environ["CART_LOGO_URL"],
-        "subtitle": f"Ваш заказ на сумму {cart_price}",
-        "default_action": {
-            "type": "web_url",
-            "url": os.environ["PIZZERIA_LOGO_URL"],
-            "webview_height_ratio": "compact",
-        },
-        "buttons": [
-            {
-                "type": "postback",
-                "title": "Самовывоз",
-                "payload": "in_cart_menu::self-delivery::0"
-            },
-            {
-                "type": "postback",
-                "title": "Доставка",
-                "payload": "in_cart_menu::delivery::0"
-            },
-            {
-                "type": "postback",
-                "title": "К меню",
-                "payload": "in_cart_menu::to_menu::0"
-            }
-        ]
-    }
-
-
-def get_cart_menu_elements(cart_items, cart_price):
-    elements = []
-    elements.append(get_cart_page(cart_price))
-    for cart_item in cart_items:
-        serialized_product_dataset = get_serialized_cart_item_dataset(cart_item)
-        elements.append(serialized_product_dataset)
-    return elements
-
-
-def send_gallery(recipient_id, elements):
-
-    params = {"access_token": os.environ["PAGE_ACCESS_TOKEN"]}
-    headers = {"Content-Type": "application/json"}
-
-    request_content = {
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": elements,
-                }
-            }
-        }
-    }
-
-    response = requests.post(
-        'https://graph.facebook.com/v2.6/me/messages',
-        params=params,
-        headers=headers,
-        json=request_content
-    )
-    response.raise_for_status()
-
-def get_menu_elements(catalogue):
-    elements = []
-    elements.append(get_main_menu())
-    for product_dataset in catalogue:
-        serialized_product_dataset = get_serialized_product_dataset(product_dataset)
-        elements.append(serialized_product_dataset)
-    elements.append(get_categories_menu())
-    return elements
 
 def facebook_handler_wrapper():
     return
+
 
 def moltin_changes_handler_wrapper():
     return
@@ -406,8 +192,8 @@ def main():
 
             facebook_handler = partial(facebook_webhook, db=db, moltin_token_dataset=moltin_token_dataset)
             moltin_changes_handler = partial(get_moltin_changes, db=db, moltin_token_dataset=moltin_token_dataset)
-            functools.update_wrapper(facebook_handler, facebook_handler_wrapper)
-            functools.update_wrapper(moltin_changes_handler, moltin_changes_handler_wrapper)
+            update_wrapper(facebook_handler, facebook_handler_wrapper)
+            update_wrapper(moltin_changes_handler, moltin_changes_handler_wrapper)
             app.add_url_rule('/', view_func=verify, methods=['GET'])
             app.add_url_rule('/', view_func=facebook_handler, methods=['POST'])
             app.add_url_rule('/changes_checker', view_func=moltin_changes_handler, methods=['POST'])
