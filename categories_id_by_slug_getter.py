129ab43ad0edea0ba2a73d7305d715cf35cc2b0b
Migheli <98918370+Migheli@users.noreply.github.com>
2022-08-03 23:41:44 +0300

    Add argparse

--- categories_id_by_slug_getter.py
+++ categories_id_by_slug_getter.py
@@ -1,16 +1,17 @@
-from moltin_api_handlers import get_token_dataset, get_category_id_by_slug
+import argparse
 import logging
+from moltin_api_handlers import get_token_dataset, get_category_id_by_slug
+
 
 logger = logging.getLogger(__name__)
 
-
 def main():
     logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('slug', type=str, help='Input slug for product category')
+    args = parser.parse_args()
     moltin_token_dataset = get_token_dataset()
-    category_slug = input(
-        'Введите slug для категории, ID которой хотите получить'
-    )
-    logger.info(get_category_id_by_slug(category_slug, moltin_token_dataset))
+    logger.info(get_category_id_by_slug(args.slug, moltin_token_dataset))
 
 
 if __name__ == '__main__':
